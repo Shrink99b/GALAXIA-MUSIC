6bd6412ce00bc69506c4acf2fa9a27983e58cde1
Shrink99b <115853073+Shrink99b@users.noreply.github.com>
2023-03-24 12:01:04 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -40,7 +40,7 @@ SONG_DOWNLOAD_DURATION = int(
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001749658510")
 
 # A name for your Music bot.
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", None)
+MUSIC_BOT_NAME = int(getenv("MUSIC_BOT_NAME", None)
 
 # Your User ID.
 OWNER_ID = list(
